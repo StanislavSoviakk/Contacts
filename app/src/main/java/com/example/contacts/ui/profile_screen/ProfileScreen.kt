4144ba8573394808e63b51e204985a61774f11d9
Stanislav_Soviak <stas.soviak@gmail.com>
2023-10-20 12:35:08 +0300

    extract string resources

--- app/src/main/java/com/example/contacts/ui/profile_screen/ProfileScreen.kt
+++ app/src/main/java/com/example/contacts/ui/profile_screen/ProfileScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.contacts.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -41,21 +43,20 @@ fun ProfileScreen(viewModel: ProfileViewModel = koinViewModel()) {
         )
         Spacer(modifier = Modifier.size(16.dp))
 
-        Text(text = "First Name: ${state.firstName}")
+        Text(text = stringResource(id = R.string.first_name, state.firstName))
         Spacer(modifier = Modifier.size(8.dp))
 
-        Text(text = "Last Name: ${state.lastName}")
+        Text(text = stringResource(id = R.string.last_name, state.lastName))
         Spacer(modifier = Modifier.size(8.dp))
 
-        Text(text = "Phone Number: ${state.phoneNumber}")
+        Text(text = stringResource(id = R.string.phone_number, state.phoneNumber))
         Spacer(modifier = Modifier.size(8.dp))
 
-        Text(text = "Email: ${state.email}")
+        Text(text = stringResource(id = R.string.email, state.email))
         Spacer(modifier = Modifier.size(8.dp))
 
-        Text(text = "Birth Date: ${state.birthDate}")
+        Text(text = stringResource(id = R.string.birth_date, state.birthDate))
         Spacer(modifier = Modifier.size(16.dp))
-
 
         Button(
             onClick = viewModel::editProfile,
@@ -63,7 +64,7 @@ fun ProfileScreen(viewModel: ProfileViewModel = koinViewModel()) {
                 .padding(vertical = 16.dp)
                 .fillMaxWidth()
         ) {
-            Text(text = "Edit Profile")
+            Text(text = stringResource(id = R.string.edit_profile))
         }
     }
 }
