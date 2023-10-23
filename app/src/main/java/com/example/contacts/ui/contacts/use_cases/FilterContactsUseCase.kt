e32f964d093db10b95d1137a9fe83b62b59d2d93
Stanislav_Soviak <stas.soviak@gmail.com>
2023-10-23 16:49:07 +0300

    refactoring

--- app/src/main/java/com/example/contacts/ui/contacts/use_cases/FilterContactsUseCase.kt
+++ app/src/main/java/com/example/contacts/ui/contacts/use_cases/FilterContactsUseCase.kt
@@ -1,18 +1,21 @@
 package com.example.contacts.ui.contacts.use_cases
 
 import com.example.contacts.base.UseCase
+import com.example.contacts.domain.model.Contact
 import com.example.contacts.ui.contacts.ContactsEvent
 import com.example.contacts.ui.contacts.ContactsState
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
 
 class FilterContactsUseCase : UseCase<ContactsEvent, ContactsState> {
     override fun canHandle(event: ContactsEvent): Boolean = event is ContactsEvent.ChangeSearchText
 
     override suspend fun invoke(event: ContactsEvent, state: ContactsState): ContactsEvent {
         if (event is ContactsEvent.ChangeSearchText) {
-            val filteredContacts = state.contactsList.filter { contact ->
+            val filteredContacts:PersistentList<Contact> = state.contactsList.filter { contact ->
                 contact.firstName.contains(state.searchText, ignoreCase = true)
                         || contact.lastName.contains(state.searchText, ignoreCase = true)
-            }
+            }.toPersistentList()
             return ContactsEvent.ContactsFiltered(contacts = filteredContacts)
         }
         throw IllegalArgumentException("Unexpected event: $event")
